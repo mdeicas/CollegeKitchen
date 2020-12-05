d66dfc2ef392fe6c3a12a322e481ccf6924008f5
Marco Deicas <marcodeicas@Marcos-MBP.home>
2020-12-05 16:06:07 -0500

    initial commit

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
 import json
 
-from db import db, Post, User
+from db import db
 from flask import Flask
 from flask import request
-from db import Asset
+from db import Asset, User, Post, Comment, Tag
 import os
 
 # can probably change the filename
@@ -33,131 +33,144 @@ def failure_response(message, code=404):
 def upload_image():
     body = json.loads(request.data)
     image_data = body.get("image_data")
-    if image_data is None:
-        return failure_response("No base64 URL to be found!")
-    asset = Asset(image_data=image_data)
+    img_type = body.get("img_type")
+    type_id = body.get("type_id")
+    # no image data or image type
+    if (image_data is None or img_type is None):
+        return failure_response("No base64 URL to be found or no image type!")
+    # incorrect image type
+    if img_type != "profile" and img_type != "recipe":
+        return failure_response(img_type)
+    asset = Asset(image_data=image_data, img_type=img_type, type_id=type_id)
     db.session.add(asset)
+    if img_type == "profile":
+        user = User.query.filter_by(id=type_id).first()
+        if user is None:
+            return failure_response("User cannot be found!")
+        user.photo.append(asset)
+    elif img_type == "post":
+        post = Post.query.filter_by(id=type_id).first()
+        if post is None:
+            return failure_response("Post cannot be found!")
+        post.photos.append(asset)
     db.session.commit()
+
     return success_response(asset.serialize(), 201)
 
-@app.route("/get_image/<int:img_id>")
+
+@app.route("/get_image/<int:img_id>/")
 def get_image(img_id):
-    asset = Asset.query.filter_by(id=img_id).first
+    asset = Asset.query.filter_by(id=img_id).first()
     if asset is None:
-      return failure_response("Image not found!")
+        return failure_response("Image not found!")
     return success_response(asset.serialize(), 200)
-
-
 
 @app.route("/getUsers/")
 def getUsers():
-	users = [u.serialize() for u in User.query.all()]
-	return success_response(users, 200)
+    users = [u.serialize() for u in User.query.all()]
+    return success_response(users, 200)
 
 
 @app.route("/user/<int:user_id>/")
 def get_User(user_id):
-	
-	#case user does not exist
-	return success_response(User.query.filter_by(id=user_id).first().serialize())
+    user = User.query.filter_by(id=user_id).first()
+    # if user is None:
+    #    return failure_response("User cannot be found!")
+    return success_response(user.serialize(), 200)
+
 
 @app.route("/register/", methods=["POST"])
 def register():
-		
-	#case user already exists
-	#case username is already taken
-	#case course is not created for whatever reason, possibly incorrect request data
 
+    # case user already exists
+    # case username is already taken
+    # case course is not created for whatever reason, possibly incorrect request data
 
-	body = json.loads(request.data)
-	user = User(
-		username=body.get("username"),
-		password=body.get("password"),
-		bio=body.get("bio"),
-		)
-	db.session.add(user)
-	db.session.commit()
-	return success_response(user.serialize(), 200)
-
-
+    body = json.loads(request.data)
+    user = User(
+        username=body.get("username"),
+        password=body.get("password"),
+        bio=body.get("bio", ""),
+    )
+    db.session.add(user)
+    db.session.commit()
+    return success_response(user.serialize(), 200)
 
 
 @app.route("/user/<int:follower_user_id>/follow/", methods=["POST"])
 def follow(follower_user_id):
-	
-	#case either user does not exist
-	#case follower id = follower id
-	#db handles case where relationship already exists, does not return error message though 
 
-	followed_user_id = json.loads(request.data).get("followed_user_id")
+    # case either user does not exist
+    # case follower id = follower id
+    # db handles case where relationship already exists, does not return error message though
 
-	follower_user = User.query.filter_by(id=follower_user_id).first()
-	follower_user.follow(followed_user_id)
-	db.session.commit()
+    followed_user_id = json.loads(request.data).get("followed_user_id")
 
-	return success_response(follower_user.serialize())
+    follower_user = User.query.filter_by(id=follower_user_id).first()
+    follower_user.follow(followed_user_id)
+    db.session.commit()
+
+    return success_response(follower_user.serialize())
 
 
 @app.route("/user/<int:follower_user_id>/unfollow/", methods=["POST"])
 def unfollow(follower_user_id):
 
-	#case either user does not exist
-	#case follower id = follower id
-	#db handles case where relationship already exists, does not return error message though 
+    # case either user does not exist
+    # case follower id = follower id
+    # db handles case where relationship already exists, does not return error message though
 
+    followed_user_id = json.loads(request.data).get("followed_user_id")
 
-	followed_user_id = json.loads(request.data).get("followed_user_id")
+    follower_user = User.query.filter_by(id=follower_user_id).first()
+    follower_user.unfollow(followed_user_id)
+    db.session.commit()
 
-	follower_user = User.query.filter_by(id=follower_user_id).first()
-	follower_user.unfollow(followed_user_id)
-	db.session.commit()
+    return success_response(follower_user.serialize())
 
-	return success_response(follower_user.serialize())
-
-#create post
+# create post
 @app.route("/user/<int:user_id>/post/", methods=["POST"])
 def post(user_id):
-	
-	#case user does not exist
-	#case post was not creeated for whatever reason, possibly because of incorrect request data
 
-	#still need to fix dateTime functionality, time functionality (only accepts ints rn)
+    # case user does not exist
+    # case post was not creeated for whatever reason, possibly because of incorrect request data
 
-	body = json.loads(request.data)
-	post = Post(
-		title=body.get("title"),
-		dateTime=body.get("dateTime"),
-		ingredients=body.get("ingredients"),
-		recipe=body.get("recipe"),
-		recipeTime=body.get("recipeTime"),
-		difficultyRating=body.get("difficultyRating"),
-		numDifficultyRatings=1,
-		overallRating=body.get("overallRating"),
-		numOverallRating=1, 
-		priceRating=body.get("priceRating"),
-		numPriceRating=1
-		)
+    # still need to fix dateTime functionality, time functionality (only accepts ints rn)
 
-	user = User.query.filter_by(id=user_id).first()
-	user.posts.append(post)
+    body = json.loads(request.data)
+    post = Post(
+        title=body.get("title"),
+        dateTime=body.get("dateTime"),
+        ingredients=body.get("ingredients"),
+        recipe=body.get("recipe"),
+        recipeTime=body.get("recipeTime"),
+        difficultyRating=body.get("difficultyRating"),
+        numDifficultyRatings=1,
+        overallRating=body.get("overallRating"),
+        numOverallRating=1,
+        priceRating=body.get("priceRating"),
+        numPriceRating=1
+    )
 
-	db.session.add(post)
-	db.session.commit()
-	
-	return success_response(post.serialize(), 200)
+    user = User.query.filter_by(id=user_id).first()
+    user.posts.append(post)
 
-#get post by post id
-#get posts by user id
-#delete post
-#rate post difficulty (we need a way to make sure than each user can only rate each post once)
-#rate post overall (we need a way to make sure than each user can only rate each post once
-#add tag to post
+    db.session.add(post)
+    db.session.commit()
 
-#add comment
-#delete comment
+    return success_response(post.serialize(), 200)
 
-#get feed -- do this later 
+# get post by post id
+# get posts by user id
+# delete post
+# rate post difficulty (we need a way to make sure than each user can only rate each post once)
+# rate post overall (we need a way to make sure than each user can only rate each post once
+# add tag to post
 
+# add comment
+# delete comment
+
+# get feed -- do this later
 
 
 if __name__ == "__main__":
--- src/db.py
+++ src/db.py
@@ -22,7 +22,9 @@ class Asset(db.Model):
     __tablename__ = "assets"
 
     id = db.Column(db.Integer, primary_key=True)
-    post_id = db.Column(db.Integer, db.ForeignKey("Posts.id"), nullable = False)
+    img_type = db.Column(db.String, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey("Posts.id"), nullable=True)
+    profile_id = db.Column(db.Integer, db.ForeignKey("Users.id"), nullable=True)
     base_url = db.Column(db.String, nullable=True)
     salt = db.Column(db.String, nullable=False)
     extension = db.Column(db.String, nullable=False)
@@ -30,22 +32,33 @@ class Asset(db.Model):
     height = db.Column(db.Integer, nullable=False)
 
     def __init__(self, **kwargs):
-        self.create(kwargs.get("image_data"), kwargs.get("post_id"))
+        self.img_type = kwargs.get("img_type")
+        if self.img_type == "profile":
+            self.profile_id = kwargs.get("type_id")
+        elif self.img_type == "post":
+            self.post_id = kwargs.get("type_id")
+
+        self.create(kwargs.get("image_data")) # create image
 
     def serialize(self):
+        if self.img_type == "post":
+            type_id = self.post_id
+        elif self.img_type == "profile":
+            type_id = self.profile_id
         return {
-            "image_id": id,
-            "post_id": self.post_id,
-            "url": f"{self.base_url}/{self.salt}.{self.extension}",
+            "image_id": self.id,
+            "img_type": self.img_type,
+            "type_id": type_id,
+            "url": f"{self.base_url}/{self.salt}.{self.extension}"
         }
-    
-    def create(self,image_data,post_id):
+
+    def create(self, image_data):
         try:
             # given a base64 string --> .png --> png
             ext = guess_extension(guess_type(image_data)[0])[1:]
             if ext not in EXTENSIONS:
                 raise Exception(f"Extension {ext} not supported!")
-            
+
             # secure way of generating random string for image name
             salt = "".join(
                 random.SystemRandom().choice(
@@ -64,7 +77,7 @@ class Asset(db.Model):
             self.extension = ext
             self.width = img.width
             self.height = img.height
-            self.post_id = post_id
+            
 
             img_filename = f"{salt}.{ext}"
             self.upload(img, img_filename)
@@ -86,169 +99,157 @@ class Asset(db.Model):
             s3_resource = boto3.resource("s3")
             object_acl = s3_resource.ObjectAcl(S3_BUCKET, img_filename)
             object_acl.put(ACL="public-read")
-            
+
             os.remove(img_temploc)
-            
+
         except Exception as e:
             print(f"Unable to upload image due to {e}")
 
 
-
-#must be here, before User model 
-followers = db.Table("Followers", 
-	db.Column("followerID", db.Integer, db.ForeignKey("Users.id")),
-	db.Column("followedID", db.Integer, db.ForeignKey("Users.id"))
-	)
+# must be here, before User model
+followers = db.Table("Followers",
+    db.Column("followerID", db.Integer, db.ForeignKey("Users.id")),
+    db.Column("followedID", db.Integer, db.ForeignKey("Users.id"))
+    )
 
 
 class User(db.Model):
-	__tablename__ = "Users"
-	id = db.Column(db.Integer, primary_key=True)
-	
-	username = db.Column(db.String, nullable=False)
-	password = db.Column(db.String, nullable=False)
-	bio = db.Column(db.String)
+    __tablename__ = "Users"
+    id = db.Column(db.Integer, primary_key=True)
 
-	#relationships
-	posts = db.relationship("Post", cascade="delete")
-	comments = db.relationship("Comment", cascade="delete")
-	#profilePics = db.relationship("Asset", cascade="delete") ####Asset rn is only for post pics 
+    username = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    bio = db.Column(db.String)
 
-
-
-	#left follows right 
-	followed = db.relationship( #users that are followed by this user 
-		"User", 
-		secondary=followers, 
-		primaryjoin=(followers.c.followerID == id), #links LHS(this user) with its entries in followers table
-		secondaryjoin=(followers.c.followedID == id), #links RHS (users this user follows) with their entries in the followers table 
-		backref=db.backref("followers", lazy="dynamic"), # "followers" is the RHS version of followed
-		lazy="dynamic")
-
-	def follow(self, followed_user_id):
-		followed_user = User.query.filter_by(id=followed_user_id).first()
-
-		if not self.is_following(followed_user):
-			self.followed.append(followed_user)
-
-	def unfollow(self, followed_user_id):
-		followed_user = User.query.filter_by(id=followed_user_id).first()
-		if self.is_following(followed_user):
-			self.followed.remove(followed_user)
-
-	def is_following(self, user):
-		return self.followed.filter(
-			followers.c.followedID == user.id).count() > 0
-
-	def getFollowersUsernames(self):
-		return [self.getUser(u.followerID).username for u in db.session.query(followers).filter(followers.c.followedID==self.id).all()]
+    posts = db.relationship("Post", cascade="delete")
+    comments = db.relationship("Comment", cascade="delete")
+    photo = db.relationship("Asset", cascade="delete")
+    followed = db.relationship(  # users that are followed by this user
+        "User",
+        secondary=followers, # links LHS(this user) with its entries in followers table
+        primaryjoin=(followers.c.followerID == id), # links RHS (users this user follows) with their entries in the followers table
+        secondaryjoin=(followers.c.followedID == id), # "followers" is the RHS version of followed
+        backref=db.backref("followers", lazy="dynamic"),
+        lazy="dynamic")
 
 
+    def follow(self, followed_user_id):
+        followed_user = User.query.filter_by(id=followed_user_id).first()
+        if not self.is_following(followed_user):
+            self.followed.append(followed_user)
 
-	def getUser(self, user_id):
-		return User.query.filter_by(id=user_id).first()
+    def unfollow(self, followed_user_id):
+        followed_user = User.query.filter_by(id=followed_user_id).first()
+        if self.is_following(followed_user):
+            self.followed.remove(followed_user)
 
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followedID == user.id).count() > 0
 
-	def serialize(self):
-		return {
-			"user_id": self.id, 
-			"username": self.username, 
-			"bio": self.bio, 
-			"posts": [p.serialize(view="user") for p in self.posts],
-			"is_following": [u.username for u in self.followed],
-			"followed_by": self.getFollowersUsernames()
-			#"followed_by": [u.username for u in db.session.query(followers).filter(followers.c.followedID==self.id).all()]
-		}
+    def getUser(self, user_id):
+        return User.query.filter_by(id=user_id).first()
 
+    def getFollowersUsernames(self):
+        return [self.getUser(u.followerID).username for u in db.session.query(followers).filter(followers.c.followedID==self.id).all()]
 
+    def serialize(self):
+        return {
+            "user_id": self.id,
+            "username": self.username,
+            "bio": self.bio,
+            "posts": [p.serialize() for p in self.posts],
+            "is_following": [u.username for u in self.followed],
+            "followed_by": self.getFollowersUsernames()
+            #"followed_by": [u.username for u in db.session.query(followers).filter(followers.c.followedID==self.id).all()]
+        }
 
 
 class Post(db.Model):
-	__tablename__ = "Posts"
-	id = db.Column(db.Integer, primary_key=True)
-	
-	title = db.Column(db.String, nullable=False)
-	dateTime = db.Column(db.String, nullable=False)
-	ingredients = db.Column(db.String, nullable=False)
-	recipe = db.Column(db.String)
-	recipeTime = db.Column(db.Integer, nullable=False)
+    __tablename__ = "Posts"
+    id = db.Column(db.Integer, primary_key=True)
 
-	difficultyRating = db.Column(db.Integer, nullable=False) #1..3 corresponds to easy medium hard
-	numDifficultyRatings = db.Column(db.Integer, nullable=False)
-	overallRating = db.Column(db.Integer, nullable=False) #integer 1..5 (5 stars)
-	numOverallRating = db.Column(db.Integer, nullable=False) 
-	priceRating = db.Column(db.Integer, nullable=False) #integer 1.. 3/4/5 (dollar signs)
-	numPriceRating = db.Column(db.Integer, nullable=False) 
+    title = db.Column(db.String, nullable=False)
+    dateTime = db.Column(db.Integer, nullable=False) # need to change to datetime later
+    ingredients = db.Column(db.String, nullable=False)
+    recipe = db.Column(db.String)
+    recipeTime = db.Column(db.Integer, nullable=False)
 
-	#foreign keys 
-	userID = db.Column(db.Integer, db.ForeignKey("Users.id"))
-
-	#relationships
-	comments = db.relationship("Comment", cascade="delete")
-	tags = db.relationship("Tag", cascade="delete")
-	#pics = db.relationship("Asset", cascade="delete")
+    # 1..3 corresponds to easy medium hard
+    difficultyRating = db.Column(db.Integer, nullable=False)
+    numDifficultyRatings = db.Column(db.Integer, nullable=False)
+    # integer 1..5 (5 stars)
+    overallRating = db.Column(db.Integer, nullable=False)
+    numOverallRating = db.Column(db.Integer, nullable=False)
+    # integer 1.. 3/4/5 (dollar signs)
+    priceRating = db.Column(db.Integer, nullable=False)
+    numPriceRating = db.Column(db.Integer, nullable=False)
 
 
-	def rateDifficulty(self, rating):
-		self.difficultyRating = ((self.difficultyRating * self.numDifficultyRatings) + rating)/(self.numDifficultyRatings + 1)
-		self.numDifficultyRatings = self.numDifficultyRatings + 1
-	def rateOverall(self, rating):
-		self.overallRating = ((self.overallRating * self.numOverallRating) + rating)/(self.numOverallRating + 1)
-		self.numOverallRating = self.numOverallRating + 1
-	def ratePrice(self, rating):
-		self.priceRating = ((self.priceRating * self.numPriceRating) + rating)/(self.numPriceRating + 1)
-		self.numPriceRating = self.numPriceRating + 1
+    userID = db.Column(db.Integer, db.ForeignKey("Users.id"))
 
-	def serialize(self):
-		return {
-			"post_id": self.id, 
-			"title": self.title, 
-			"dateTime": self.dateTime, 
-			"ingredients": self.ingredients, 
-			"recipe": self.recipe, 
-			"recipeTime": self.recipeTime, 
-			
-			"difficultyRating": self.difficultyRating,
-			"overallRating": self.overallRating, 
-			"priceRating": self.priceRating, 
+    comments = db.relationship("Comment", cascade="delete")
+    tags = db.relationship("Tag", cascade="delete")
+    photos = db.relationship("Asset", cascade="delete")
 
-			"user_id": self.userID,
-			"comments": [c.serialize(view="post") for c in self.comments],
-			"tags": [t.serialize(view="post") for t in self.tags]#,
+    def rateDifficulty(self, rating):
+        self.difficultyRating = ((self.difficultyRating * self.numDifficultyRatings) + rating)/(self.numDifficultyRatings + 1)
+        self.numDifficultyRatings = self.numDifficultyRatings + 1
+    def rateOverall(self, rating):
+        self.overallRating = ((self.overallRating * self.numOverallRating) + rating)/(self.numOverallRating + 1)
+        self.numOverallRating = self.numOverallRating + 1
+    def ratePrice(self, rating):
+        self.priceRating = ((self.priceRating * self.numPriceRating) + rating)/(self.numPriceRating + 1)
+        self.numPriceRating = self.numPriceRating + 1
 
-			#"pics": [p.serialize for p in self.pics]
-		}
+    def serialize(self):
+        return {
+            "post_id": self.id, 
+            "title": self.title, 
+            "dateTime": self.dateTime, 
+            "ingredients": self.ingredients, 
+            "recipe": self.recipe, 
+            "recipeTime": self.recipeTime, 
+            
+            "difficultyRating": self.difficultyRating,
+            "overallRating": self.overallRating, 
+            "priceRating": self.priceRating, 
+
+            "user_id": self.userID,
+            "comments": [c.serialize(view="post") for c in self.comments],
+            "tags": [t.serialize(view="post") for t in self.tags],
+
+            "photos": [p.serialize for p in self.photos]
+        }
 
 class Comment(db.Model):
-	__tablename__ = "Comments"
-	id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "Comments"
+    id = db.Column(db.Integer, primary_key=True)
 
-	comment = db.Column(db.String, nullable=False)
+    comment = db.Column(db.Integer, nullable=False)
 
-	#foreign keys
-	userID = db.Column(db.Integer, db.ForeignKey("Users.id"))
-	postID = db.Column(db.Integer, db.ForeignKey("Posts.id"))
+    userID = db.Column(db.Integer, db.ForeignKey("Users.id"))
+    postID = db.Column(db.Integer, db.ForeignKey("Posts.id"))
 
-	def serialize(self):
-		return {
-		"comment_id": self.id, 
-		"comment": self.comment, 
-		"user_id": self.userID, 
-		"post_id": self.postID
-		}
+    def serialize(self):
+        return {
+        "comment_id": self.id, 
+        "comment": self.comment, 
+        "user_id": self.userID, 
+        "post_id": self.postID
+        }
 
 class Tag(db.Model):
-	__tablename__ = "Tags"
-	id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "Tags"
+    id = db.Column(db.Integer, primary_key=True)
 
-	tag = db.Column(db.String, nullable=False)
+    tag = db.Column(db.Integer, nullable=False)
 
-	#foreign keys 
-	postID = db.Column(db.Integer, db.ForeignKey("Posts.id"))
+    postID = db.Column(db.Integer, db.ForeignKey("Posts.id"))
 
-	def serialize(self):
-		return {
-		"tag_id": self.id, 
-		"tag": self.tag, 
-		"post_id": self.postID
-		}
+    def serialize(self):
+        return {
+        "tag_id": self.id, 
+        "tag": self.tag, 
+        "post_id": self.postID
+        }
